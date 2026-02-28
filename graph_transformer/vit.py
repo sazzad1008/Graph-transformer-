Fix attention scaling per head
Co-authored-by: sazzad1008 <103903789+sazzad1008@users.noreply.github.com>

graph_transformer/vit.py
@@ -36,6 +36,7 @@ class MultiHeadAttention(nn.Module):
         super().__init__()
         self.emb_size = emb_size
         self.num_heads = num_heads
+        self.d_k = emb_size // num_heads
         self.keys = nn.Linear(emb_size, emb_size)
         self.queries = nn.Linear(emb_size, emb_size)
         self.values = nn.Linear(emb_size, emb_size)
@@ -50,7 +51,7 @@ class MultiHeadAttention(nn.Module):
         if mask is not None:
             fill_value = torch.finfo(torch.float32).min
             energy = energy.masked_fill(~mask, fill_value)
-        scaling = self.emb_size ** (1 / 2)
+        scaling = self.d_k**0.5
         att = F.softmax(energy / scaling, dim=-1)
         att = self.att_drop(att)
         out = torch.einsum("b h a l, b h l v -> b h a v", att, values)